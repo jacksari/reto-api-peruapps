73261dca154930f1260eb4a4331fa2785c796d4f
Jack Sari <janasarii@gmail.com>
2021-10-03 21:23:30 -0500

    update key certbot

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,8 +17,8 @@ module.exports = {
             "NODE_ENV": "production",
             "MONGO_DB": "mongodb+srv://jacksari:Shingekino123@cluster0.nclr5.mongodb.net/reto-peru",
             "JWT_SECRET": "JWT_SECRET",
-            "KEY_PATH": "/etc/letsencrypt/live/api.jacksari.com/fullchain.pem",
-            "CERT_PATH": "/etc/letsencrypt/live/api.jacksari.com/privkey.pem",
+            "KEY_PATH": "/etc/letsencrypt/live/api.jacksari.com/privkey.pem",
+            "CERT_PATH": "/etc/letsencrypt/live/api.jacksari.com/fullchain.pem",
         }
       }
   ]
